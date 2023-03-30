b72bec89cb34cf7fafd583adfc8381b41ba6c3e1
Siasor88 <ssrahimi2@gmail.com>
2023-03-30 12:40:36 +0330

    meow

--- Main.py
+++ Main.py
@@ -1,1 +1,27 @@
-print("meow")
+
+from enum import Enum
+
+
+class TokenType(Enum):
+    NUM = 'NUM'
+    ID = 'ID'
+    KEYWORD = 'KEYWORD'
+    SYMBOL = 'SYMBOL'
+    COMMENT = 'COMMENT'
+    WHITESPACE = 'WHITESPACE'
+
+
+class Token:
+    def __init__(self, type: TokenType, string: str):
+        self.type = type
+        self.string = string
+
+    def __str__(self):
+        return f'<{self.type}, {self.string}>'
+
+
+
+def get_next_token():
+    pass
+
+
