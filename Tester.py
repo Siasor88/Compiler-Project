75b1235eec9212d489c74d5d3ab1b236af04bacf
Siasor88 <ssrahimi2@gmail.com>
2023-04-04 00:01:37 +0330

    meow

--- Tester.py
+++ Tester.py
@@ -1,16 +1,25 @@
 import re
-Tests_num = 10
-for i in range(1,Tests_num+1):
-    print("Test", i, "of", Tests_num)
-    f1 = open('./PA1_testcases/T' + ('0'+str(i) if i < 10 else str(i)) + '/lexical_errors.txt', 'r')
-    f2 = open('./PA1_testcases/T' + ('0'+str(i) if i < 10 else str(i)) + '/result_lexical_errors.txt', 'r')
-    print("Lexical Errors:")
 
+
+def get_symbol(symbol: str):
+    p = re.compile('\d+\.\s+(\w+)')
+    return p.search(symbol).group(1)
+
+
+Tests_num = 10
+passed_tests = 0
+for i in range(1, Tests_num + 1):
+    test_state = True
+    print("Test", i, "of", Tests_num)
+    f1 = open('./PA1_testcases/T' + ('0' + str(i) if i < 10 else str(i)) + '/lexical_errors.txt', 'r')
+    f2 = open('./PA1_testcases/T' + ('0' + str(i) if i < 10 else str(i)) + '/result_lexical_errors.txt', 'r')
+    print("Lexical Errors:")
     if re.sub(r'\s+', '', f1.read()) == re.sub(r'\s+', '', f2.read()):
         print("Passed")
     else:
         print("Failed")
-        #print the different lines between the two files with the line numbers
+        test_state = False
+        # print the different lines between the two files with the line numbers
         f1.seek(0)
         f2.seek(0)
         line_num = 1
@@ -20,14 +29,15 @@ for i in range(1,Tests_num+1):
             line_num += 1
     f1.close()
     f2.close()
-    f1 = open('./PA1_testcases/T' + ('0'+str(i) if i < 10 else str(i)) + '/tokens.txt', 'r')
-    f2 = open('./PA1_testcases/T' + ('0'+str(i) if i < 10 else str(i)) + '/result_tokens.txt', 'r')
+    f1 = open('./PA1_testcases/T' + ('0' + str(i) if i < 10 else str(i)) + '/tokens.txt', 'r')
+    f2 = open('./PA1_testcases/T' + ('0' + str(i) if i < 10 else str(i)) + '/result_tokens.txt', 'r')
     print("Tokens:")
     if re.sub(r'\s+', '', f1.read()) == re.sub(r'\s+', '', f2.read()):
         print("Passed")
     else:
         print("Failed")
-        #print the different lines between the two files with the line numbers
+        test_state = False
+        # print the different lines between the two files with the line numbers
         f1.seek(0)
         f2.seek(0)
         line_num = 1
@@ -37,4 +47,24 @@ for i in range(1,Tests_num+1):
             line_num += 1
     f1.close()
     f2.close()
+
+    f1 = open('./PA1_testcases/T' + ('0' + str(i) if i < 10 else str(i)) + '/symbol_table.txt', 'r')
+    f2 = open('./PA1_testcases/T' + ('0' + str(i) if i < 10 else str(i)) + '/result_symbol_table.txt', 'r')
+
+    f1_read = f1.read().strip()
+    f2_read = f2.read().strip()
+
+    f1_elements = f1_read.split('\n')
+    f2_elements = f2_read.split('\n')
+    f1_elements = set([get_symbol(e) for e in f1_elements])
+    f2_elements = set([get_symbol(e) for e in f2_elements])
+    print('Symbols:')
+    if f1_elements == f2_elements:
+        print("Passed")
+    else:
+        print("Failed")
+        test_state = False
     print("__________________________________________________________")
+    if test_state:
+        passed_tests += 1
+print(f'{passed_tests}/{Tests_num} test passed.')
